34ccd886057298a843ffe7c313b14f0f1bb6246b
apetrenko <apetrenko@instartlogic.com>
2019-06-26 21:59:13 +0300

    homework ready initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TagsInputComponent } from './components/tags-input/tags-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagsInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
